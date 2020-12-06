1c3b9d7f20dbacc098e7e14586cda4967abfb729
kyawmyotun <kyawmyotun472001@gmail.com>
2020-12-06 10:35:54 +0630

    complete register email

--- packages/web/components/register/EmailRegister.tsx
+++ packages/web/components/register/EmailRegister.tsx
@@ -1,20 +1,72 @@
+import { FacebookOutlined, GoogleOutlined } from '@ant-design/icons';
+import { Button, Col, Divider, Form, Input, Row } from 'antd';
 import Layout, { Content } from 'antd/lib/layout/layout';
 import Sider from 'antd/lib/layout/Sider';
-import Image from 'next/image';
-import { FC } from 'react';
+import React, { FC } from 'react';
+
+const Item = Form.Item;
 export const EmailRegister: FC = () => {
   return (
-    <Layout>
-      <Sider>
-        <Image
-          src="/registerEmail.jpg"
-          alt="Picture of the author"
-          width={200}
-          height={850}
-          layout={'fixed'}
-        ></Image>
+    <Layout color="white">
+      <Sider width="156px">
+        <img src="/registerEmail.jpg"></img>
       </Sider>
-      <Content></Content>
+      <Content>
+        <Row>
+          <Col style={{ marginTop: '100px', marginLeft: '20px' }}>
+            <Row>
+              <span style={{ fontSize: 30, fontWeight: 'bold' }}>SingUp</span>
+            </Row>
+            <Row>
+              <p>
+                By continuing, you agree to our UserAgreement and
+                <br></br>
+                Privacy Policy. CONTINUE
+              </p>
+            </Row>
+            <Row style={{ marginTop: '100px' }}>
+              <Button
+                type="primary"
+                className="emailRegisterField"
+                icon={<GoogleOutlined />}
+                size={'large'}
+              >
+                Continue With Google
+              </Button>
+            </Row>
+            <Row style={{ marginTop: '30px' }}>
+              <Button
+                className="emailRegisterField"
+                icon={<FacebookOutlined />}
+                size={'large'}
+              >
+                Continue With FaceBook
+              </Button>
+            </Row>
+            <Divider dashed> OR </Divider>
+            <Row style={{ marginTop: '30px' }}>
+              <Form>
+                <Item
+                  label="Email"
+                  name="email"
+                  className="emailRegisterFieldInput"
+                >
+                  <Input />
+                </Item>
+              </Form>
+            </Row>
+            <Row>
+              <Button
+                size="large"
+                type="primary"
+                className="emailRegisterFieldInput"
+              >
+                Continue
+              </Button>
+            </Row>
+          </Col>
+        </Row>
+      </Content>
     </Layout>
   );
 };
---
+++ packages/web/pages/dist/register.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+var components_1 = require("../components");
+var Register = function () {
+    return (react_1["default"].createElement("div", null,
+        react_1["default"].createElement(components_1.EmailRegister, null)));
+};
+exports["default"] = Register;
