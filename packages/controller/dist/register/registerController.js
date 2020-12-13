92d603b48cf48b5c99616abbc4db51b5cd15015a
kyawmyotun <kyawmyotun472001@gmail.com>
2020-12-13 15:43:27 +0630

    :tada: |

--- packages/controller/dist/register/registerController.js
+++ packages/controller/dist/register/registerController.js
@@ -1,6 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RegisterController = void 0;
-var RegisterController = function () { };
+var formik_1 = require("formik");
+var react_1 = __importDefault(require("react"));
+var RegisterController = function (_a) {
+    var children = _a.children;
+    return (react_1.default.createElement(formik_1.Formik, { initialValues: {
+            username: '',
+            email: '',
+            displayName: '',
+            password: '',
+        }, onSubmit: function () {
+            console.log('Submited');
+        } }, function (props) { return children(props); }));
+};
 exports.RegisterController = RegisterController;
 //# sourceMappingURL=registerController.js.map
---
+++ packages/controller/dist/types.d.ts
@@ -0,0 +1,7 @@
+export interface RegisterFormValues {
+    username: string;
+    email: string;
+    displayName: string;
+    password: string;
+}
+//# sourceMappingURL=types.d.ts.map
