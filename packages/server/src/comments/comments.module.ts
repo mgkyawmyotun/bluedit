bdece225893743334ec62855d17375a22828b0c7
kyawmyotun <kyawmyotun472001@gmail.com>
2020-12-25 10:19:48 +0630

    create comments mutation

--- packages/server/src/comments/comments.module.ts
+++ packages/server/src/comments/comments.module.ts
@@ -2,11 +2,14 @@ import { CacheModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as redisStore from 'cache-manager-redis-store';
 import { CommentEntity } from './comments.entity';
+import { CommentsService } from './comments.service';
+import { CommentsResolver } from './comments.resolver';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([CommentEntity]),
     CacheModule.register({ store: redisStore }),
   ],
+  providers: [CommentsService, CommentsResolver],
 })
 export class CommentsModule {}
---
+++ packages/server/src/comments/comments.types.ts
@@ -0,0 +1,26 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { ErrorInterface } from '../shared/types';
+import { User } from '../users/users.type';
+
+@ObjectType()
+export class Comment {
+  @Field(type => String)
+  comment_text: string;
+  @Field(type => User)
+  user: User;
+}
+
+@ObjectType({ implements: () => [ErrorInterface] })
+export class CommentError implements ErrorInterface {
+  path: string;
+  message: string;
+}
+
+@InputType()
+export class CommentInput {
+  @Field(type => String)
+  comment_text: string;
+
+  @Field(type => String)
+  post_id: string;
+}
