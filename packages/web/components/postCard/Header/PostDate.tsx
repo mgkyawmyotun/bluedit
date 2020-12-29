c805d1e7dc9055566bebb70597c604141fc94129
kyawmyotun <kyawmyotun472001@gmail.com>
2020-12-29 12:33:04 +0630

    add tooltip

--- packages/web/components/postCard/Header/PostDate.tsx
+++ packages/web/components/postCard/Header/PostDate.tsx
@@ -1,7 +1,14 @@
+import { Tooltip } from 'antd';
 import { FC } from 'react';
 import styles from './../../../styles/postCard.module.css';
 
 interface PostDateProps {}
 export const PostDate: FC = () => {
-  return <span className={styles.header__date}>2 hour ago</span>;
+  return (
+    <span className={styles.header__date}>
+      <Tooltip placement={'top'} title={new Date().getFullYear()}>
+        2 hour ago
+      </Tooltip>
+    </span>
+  );
 };
