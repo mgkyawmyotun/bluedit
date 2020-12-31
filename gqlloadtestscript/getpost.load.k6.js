fdcaa09a985e21007755b5cf768271b9efad9b45
kyawmyotun <kyawmyotun472001@gmail.com>
2020-12-31 16:47:23 +0630

    add cache layer on top of post service

--- gqlloadtestscript/getpost.load.k6.js
+++ gqlloadtestscript/getpost.load.k6.js
@@ -1,31 +1,9 @@
 import http from 'k6/http';
 
-const query = `
-query getPosts {
-  getPosts {
-    post_id
-    post_text
-    title
-    link
-    sub{
-      name
-    }
-    vote_count
-    images
-    videos
- 	 user {
-    username
-    displayName
-    email
-  }
-    comment_count
-  }
-}
-`;
 export default function () {
   let jar = http.cookieJar();
   jar.set(
-    'http://localhost:4000/graphql',
+    'http://localhost:3000',
     'bid',
     's%3AO1vBW9p1OAcdvC9fXHas6G3HpqUhzPwy.KzxL5fp%2BZE%2FEt9zhoqcQJa1%2Bsba5FiksYfi5KedAdxs',
     {
@@ -34,8 +12,17 @@ export default function () {
       secure: false,
     }
   );
-  let res = http.post(
-    'http://localhost:4000/graphql',
-    JSON.stringify({ query: query })
-  );
+  const data = {
+    operationName: 'getPosts',
+    query:
+      'query getPosts {↵  getPosts {↵    post_id↵    post_text↵    title↵    link↵    sub {↵      name↵      __typename↵    }↵    vote_count↵    images↵    videos↵    user {↵      username↵      __typename↵    }↵    comment_count↵    __typename↵  }↵}↵',
+    variables: {},
+  };
+  let res = http.post('http://localhost:4000/graphql', JSON.stringify(data), {
+    cookies: {
+      bid:
+        's%3AO1vBW9p1OAcdvC9fXHas6G3HpqUhzPwy.KzxL5fp%2BZE%2FEt9zhoqcQJa1%2Bsba5FiksYfi5KedAdxs',
+    },
+  });
+  console.log(JSON.stringify(res));
 }
--- packages/server/src/posts/posts.service.ts
+++ packages/server/src/posts/posts.service.ts
@@ -1,19 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Cache } from 'cache-manager';
 import { Repository } from 'typeorm';
 import { PostEntity } from '../posts/posts.entity';
 
 @Injectable()
 export class PostsService {
+  private getPostCacheKey = 'post/getPosts';
   constructor(
     @InjectRepository(PostEntity)
     private postRepository: Repository<PostEntity>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
+  private logger: Logger = new Logger('postService');
 
   public async getPosts() {
+    const postsCache = await this.cacheManager.get<PostEntity[]>(
+      this.getPostCacheKey,
+    );
+    if (postsCache) {
+      return postsCache;
+    }
     const posts = await this.postRepository.find({
       relations: ['sub', 'user'],
     });
+    this.cacheManager.set(this.getPostCacheKey, posts, { ttl: 60 * 60 * 24 });
     return posts;
   }
 }
